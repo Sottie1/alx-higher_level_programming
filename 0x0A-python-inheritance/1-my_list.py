525dc37fc809547aa36a3f70c9c59d393d20bc47
Sottie1 <sottiegadson@gmail.com>
2022-08-01 13:39:05 -0700

    Making files executable

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,10 +8,3 @@ class MyList(list):
     def print_sorted(self):
         """Print a list in sorted ascending order."""
         print(sorted(self))
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Stat
